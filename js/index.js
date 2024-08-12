530b67d0fb8020779d818f8be83b31dabfc26792
mustafaa <aushev.mustafa.ru@mail.ru>
2024-08-12 14:14:22 +0300

    js1

---
+++ js/index.js
@@ -0,0 +1,370 @@
+// Объявление переменной
+// // 1. Объявите переменную name и присвойте ей ваше имя.
+
+// const name="mustafa"
+// console.log(name);
+
+// Объявление нескольких переменных
+// 2. Объявите переменные age и city, присвойте им значения.
+
+// let age=25
+// let city="ali-yrt"
+// console.log(age,city);
+
+// Конкатенация строк
+// 3. Объявите переменные firstName и lastName, объедините их и выведите полное имя.
+
+// let firstName="mustafa"
+// let lastName="yahya"
+// let result=`${firstName} ${lastName}`
+// console.log(result); 
+
+
+// Переменные и типы данных
+// 4. Объявите переменную isStudent и присвойте ей значение true.
+
+// let isStudent=true
+// console.log(isStudent);
+
+
+// // Изменение значения переменной
+// // 5. Объявите переменную count, присвойте ей значение, затем измените это значение.
+
+// let count="mustafa"
+// count="yahya"
+// console.log(count);
+
+// // Обмен значений переменных
+// // 6. Объявите переменные a и b, поменяйте их значения местами.
+
+// let a=10
+// let b=20
+// a=20
+// b=10
+// console.log(a,b);
+
+
+// // Проверка типа переменной
+// // 7. Объявите переменную price и присвойте ей значение 19.99. Проверьте её тип.
+
+// let price=19.99
+// console.log(typeof price);
+
+
+// // Использование const
+// // 8. Объявите константу PI и присвойте ей значение 3.14.
+
+// const PI=3.14
+// console.log(PI);
+
+
+// // Преобразование строки в число
+// // 9. Объявите переменную str со значением "123" и преобразуйте её в число.
+
+// let str="123"
+// str=123
+// console.log(str);
+
+
+// // Преобразование числа в строку
+// // 10. Объявите переменную num со значением 456 и преобразуйте её в строку.
+
+// let num=456
+// num="456"
+// console.log(num);
+
+
+// // Объявление массива
+// // 11. Объявите массив colors с элементами "red", "green", "blue".
+
+// const colors=["red","green","blue"]
+// console.log(colors);
+
+
+// // Добавление элемента в массив
+// // 12. Добавьте элемент "yellow" в массив colors.
+
+// const colors=["red","green","blue"]
+// colors.push("yellow")
+// console.log(colors);
+
+
+// // Удаление элемента из массива
+// // 13. Удалите последний элемент из массива colors.
+
+// const colors=["red","green","blue"]
+// colors.pop("blue")
+// console.log(colors);
+
+
+// // Доступ к элементу массива
+// // 14. Объявите массив fruits с элементами "apple", "banana", "cherry". Выведите второй элемент массива.
+
+// let fruits=["apple","banana","cherry"]
+// console.log(fruits[1]);
+
+
+// // Объявление объекта
+// // 15. Объявите объект person с свойствами name и age.
+
+// let person={}
+//     person.age=24
+//     person.name="mustafa"
+
+// console.log(person);
+
+
+// // Доступ к свойству объекта
+// // 16. Объявите объект car с свойствами make и model. Выведите значение свойства make.
+
+// let car={}
+// car.make="bmw"
+// car.modal="mercedes"
+// console.log(car.make);
+
+
+// // Изменение свойства объекта
+// // 17. Измените значение свойства age в объекте person.
+
+// let person={}
+// person.age=24
+// person.name="mustafa"
+// person.age=25
+// console.log(person.age);
+
+
+// Добавление свойства к объекту
+// 18. Добавьте свойство color к объекту car.
+
+// let car={}
+// car.color=('blue')
+// console.log(car.color);
+
+
+// Удаление свойства из объекта
+// 19. Удалите свойство model из объекта car.
+
+// let car={}
+// car.make="bmw"
+// car.model="mercedes"
+// delete car["model"]
+// console.log(car);
+
+// Использование шаблонных строк
+// 20. Объявите переменные firstName и age. Используйте шаблонные строки для вывода фразы "My name is [firstName] and I am [age] years old."
+
+// let firstName="mustafa"
+// let age=25
+// let result = `my name is ${firstName} and I am ${age} yers old`
+// console.log(result);
+
+// Объявление булевой переменной
+// 21. Объявите переменную isAvailable и присвойте ей значение false.
+
+// let isAvailable=false
+// console.log(isAvailable);
+
+
+// Использование оператора typeof
+// 22. Объявите переменные x и y со значениями 10 и "hello" соответственно. Используйте typeof для проверки их типов.
+
+// let x=10
+// let y="hello"
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+// Сравнение переменных
+// 23. Объявите переменные a и b со значениями 5 и 10. Используйте оператор == для проверки их равенства.
+
+// let a=5
+// let b=10
+// console.log(a==b);
+
+// Строгое сравнение переменных
+// 24. Объявите переменные a и b со значениями "5" и 5. Используйте оператор === для проверки их строгого равенства.
+
+// Логические операторы
+// 25. Объявите переменные isAdult и hasTicket. Используйте логический оператор && для проверки, что обе переменные истинны.
+
+// Логические операторы
+// 25. Объявите переменные isAdult и hasTicket. Используйте логический оператор && для проверки, что обе переменные истинны.
+
+// let isAdult=true
+// let hasTicket=true
+// console.log(isAdult && hasTicket);
+
+// Объявление функции
+// 26. Объявите функцию greet, которая принимает параметр name и выводит приветствие с этим именем.
+
+// function greet(name){
+//     let namegreet=`приветствие,${name}`;
+//     console.log(namegreet); 
+// }
+// greet("mustafa")
+
+// 27. Объявите функцию square, которая принимает число и возвращает его квадрат.
+
+// function square(num){
+//     return num*num;
+// }
+// console.log(square(24));
+
+// Функция с параметрами по умолчанию
+// 28. Объявите функцию multiply с двумя параметрами, второй параметр должен иметь значение по умолчанию 1.
+
+// function multiply(one,two=1){
+//     return one*two
+// }
+// console.log(multiply);
+
+// Конкатенация чисел и строк
+// 29. Объявите переменные num и str со значениями 10 и "20" соответственно. Сконкатенируйте их и выведите результат.
+
+// let num=10
+// let str="20"
+// console.log(num+str);
+
+
+// Преобразование boolean в строку
+// 30. Объявите переменную isHappy со значением true и преобразуйте её в строку.
+
+// let isHappy=true
+// isHappy=String(isHappy)
+// console.log(isHappy);
+
+// Массив строк в одну строку
+// 31. Объявите массив строк и объедините их в одну строку, используя метод join.
+
+// let ad=["str","str","str","str","str","str"]
+// let resultad=ad.join();
+// console.log(ad);
+
+
+// Преобразование строки в массив символов
+// 32. Объявите строку и преобразуйте её в массив символов.
+
+// let stroka="masive"
+// let arr=stroka.split("######")
+// console.log(stroka);    
+
+
+// Преобразование строки в массив символов
+// // 32. Объявите строку и преобразуйте её в массив символов.
+// let text="mustafa"
+// let textmustafa=text.length
+// console.log(textmustafa);
+
+// Объявление многомерного массива
+// 34. Объявите массив массивов (двумерный массив).
+
+// const mordovorot=[["musa","isa"],["tima","mira"]]
+// console.log(mordovorot);
+
+
+// Доступ к элементу многомерного массива
+// 35. Объявите двумерный массив и выведите элемент из второй строки и первого столбца.
+
+// const mordovorot=[
+//     ["musa","isa"],
+//     ["tima","mira"]
+// ]
+// console.log(mordovorot[1][0]);
+
+// Создание объекта с методами
+// 36. Объявите объект с методами greet и farewell. Каждый метод должен выводить сообщение.
+
+// const mordovorot =  {
+//     greet(){
+//         console.log("greet");
+//     },
+//     farewell(){
+//         console.log("farewell");
+//     }
+// }
+// mordovorot.greet()
+// mordovorot.farewell()
+
+
+// Вызов метода объекта
+// 37. Вызовите метод greet из объекта.
+
+// mordovorot.greet()
+// console.log(mordovorot.greet)
+
+// 38. Проверьте, существует ли свойство name в объекте person.
+// console.log("name" in person);
+
+// Клонирование объекта
+// 39. Создайте копию объекта person с помощью оператора расширения.
+
+// let person123={...person};
+
+// Объединение объектов
+// 40. Объедините два объекта в один, используя оператор расширения.
+
+// let person234={...person,...person123};
+
+// Объявление переменной с undefined
+// 41. Объявите переменную x без присваивания значения.
+
+// let x
+
+// Проверка переменной на undefined
+// 42. Проверьте, является ли переменная x undefined.
+
+// console.log(x);
+
+// Присваивание null переменной
+// 43. Объявите переменную y и присвойте ей значение null.
+
+// let y =null
+
+// let dempsy=null
+
+// 44. Проверьте, является ли переменная y null.
+// console.log(dempsy);
+
+// Изменение значения элемента массива
+// 45. Объявите массив numbers и измените значение первого элемента.
+
+// let numbers=[1,2,3,4,5];
+// numbers[0]=2;
+
+// Использование метода массива
+// 46. Объявите массив и используйте метод forEach для вывода каждого элемента.
+
+// let masive2=[1,2,3];
+// masive2.forEach((element)=>console.log(element));
+
+// Преобразование числа в строку и обратно
+// 47. Объявите переменную num со значением 100. Преобразуйте её в строку, затем обратно в число.
+
+// let num=100
+// num=String(num);
+// num=Number(num)
+
+// Сравнение строк
+// 48. Объявите переменные str1 и str2 со значениями "apple" и "banana". Сравните их.
+
+// let str1="apple"
+// let str2="banana"
+// console.log(str1===str2);
+
+// Объявление массива чисел
+// 49. Объявите массив numbers с несколькими элементами и выведите его длину.
+
+// let numbers=[1,2,3,4,5]
+// console.log(numbers.length);
+
+// Объявление объекта с вложенными объектами
+// 50. Объявите объект student с вложенными объектами address и grades.
+
+// let student = {
+//     adress: {
+//       city: Nazran,
+//     },
+//     grades: {
+//       grades: 5,
+//     },
+//   };
